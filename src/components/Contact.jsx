e9008bd71769b00307c173a010389d6b468441ba
vinayak.wagh70 <vinayak.wagh70@gmail.com>
2026-04-26 16:36:25 +0300

    Social Media links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,6 +15,25 @@ export default function Contact() {
       <p style={{ marginTop: "15px" }}>
         Business Account: Villa Revontuli
       </p>
+
+      <p style={{ marginTop: "20px" }}>
+        Follow us:
+      </p>
+
+      <div className="social-links">
+        <a href="https://instagram.com/villarevontuli?igsh=ZGIzdWkxamEyMGQx" target="_blank">
+          <img 
+            src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" 
+            className="social-icon" 
+          />
+        </a>
+        <a href="https://share.google/PFNfb6CEZKT5DVCs0" target="_blank">
+          <img 
+            src="https://cdn-icons-png.flaticon.com/512/281/281764.png" 
+            className="social-icon" 
+          />
+        </a>
+      </div>
     </section>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,22 @@
 export default function Footer() {
   return (
     <footer>
-      © {new Date().getFullYear()} Villa Revontuli · Mahabaleshwar
+      <p>© {new Date().getFullYear()} Villa Revontuli · Mahabaleshwar</p>
+
+      <div className="social-links">
+        <a href="https://instagram.com/villarevontuli?igsh=ZGIzdWkxamEyMGQx" target="_blank">
+          <img 
+            src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" 
+            className="social-icon" 
+          />
+        </a>
+        <a href="https://share.google/PFNfb6CEZKT5DVCs0" target="_blank">
+          <img 
+            src="https://cdn-icons-png.flaticon.com/512/281/281764.png" 
+            className="social-icon" 
+          />
+        </a>
+      </div>
     </footer>
   );
 }
