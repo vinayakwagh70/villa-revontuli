2a65a6136e80831f2283bfdb1a08806a75ab2976
vinayak.wagh70 <vinayak.wagh70@gmail.com>
2026-05-14 17:42:58 +0300

    modified Hero texts

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,8 +25,8 @@ export default function Hero() {
       }}
     >
       <div className="hero-content">
-        <h1>Villa in Mahabaleshwar – Villa Revontuli</h1>
-        <p>Peaceful private villa stay Mahabaleshwar</p>
+        <h1>Villa Revontuli</h1>
+        <p>Peaceful private villa stay in Mahabaleshwar</p>
 
         <a
           className="button"
