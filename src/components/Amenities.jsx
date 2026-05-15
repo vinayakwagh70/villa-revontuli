9bd35463f9e12bbd7653f1a839c591ed4e47df2d
vinayak.wagh70 <vinayak.wagh70@gmail.com>
2026-05-15 20:46:26 +0300

    updated amenities

--- src/components/Amenities.jsx
+++ src/components/Amenities.jsx
@@ -4,11 +4,13 @@ export default function Amenities() {
       <h2>Comfort & Amenities</h2>
       <div className="grid grid-3">
         <div className="card">2 Bedrooms with attached bathrooms</div>
-        <div className="card">Peaceful mountain surroundings</div>
+        <div className="card">1 Common Toilet</div>
         <div className="card">Microwave & kettle</div>
         <div className="card">Hot water in all bathrooms</div>
         <div className="card">WiFi available</div>
         <div className="card">Power backup</div>
+        <div className="card">Free Parking</div>
+        <div className="card">Peaceful mountain surroundings</div>
       </div>
     </section>
   );
